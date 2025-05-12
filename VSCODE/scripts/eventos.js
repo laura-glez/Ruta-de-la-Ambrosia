cfb5385ee04be02100035f88a6dd76d36a123152
gegetako <gemamartose@gmail.com>
2025-05-12 16:45:26 +0200

    Datos de tabla añadidos desde json sin problemas,tabla de eventos realizada y con sus respectivos estilos

--- VSCODE/scripts/eventos.js
+++ VSCODE/scripts/eventos.js
@@ -1,48 +1,18 @@
-const tabla = document.getElementById('tabla');
-const eventos =[
-{
-    id: "01",
-    nombre: "CaVinos01",
-    estado: "activo",
-    aforo: "25"
-},
-{
-    id: "02",
-    nombre: "CaVinos02",
-    estado: "activo",
-    aforo: "30"
-},
-{
-    id: "03",
-    nombre: "CaCerveza01",
-    estado: "activo",
-    aforo: "25"
-},
-{
-    id: "04",
-    nombre: "CaCerveza02",
-    estado: "activo",
-    aforo: "30"
-},
-{
-    id: "05",
-    nombre: "CaCocktails01",
-    estado: "activo",
-    aforo: "25"
-},
-{
-    id: "06",
-    nombre: "CaCocktails02",
-    estado: "activo",
-    aforo: "30"
-}
+const tabla = document.getElementById("tabla").querySelector("tbody");
+
+const eventos = [
+    { id: "01", nombre: "CaVinos01", estado: "activo", aforo: "25" },
+    { id: "02", nombre: "CaVinos02", estado: "activo", aforo: "30" },
+    { id: "03", nombre: "CaCerveza01", estado: "activo", aforo: "25" },
+    { id: "04", nombre: "CaCerveza02", estado: "activo", aforo: "30" },
+    { id: "05", nombre: "CaCocktails01", estado: "activo", aforo: "25" },
+    { id: "06", nombre: "CaCocktails02", estado: "activo", aforo: "30" }
 ];
 
-function renderTabla (events){
-    const tabla = document.getElementById("tabla").getElementsByTagName("tbody")[1];
-    tabla.innerHTML = "";
+function renderTabla(events) {
     events.forEach(e => {
-        const fila = tabla.insertRow ();
+        const fila = tabla.insertRow(); 
+
         const celdaId = fila.insertCell();
         celdaId.textContent = e.id;
 
@@ -56,6 +26,5 @@ function renderTabla (events){
         celdaAforo.textContent = e.aforo;
     });
 }
-renderTabla(eventos);
 
-  
+renderTabla(eventos);
