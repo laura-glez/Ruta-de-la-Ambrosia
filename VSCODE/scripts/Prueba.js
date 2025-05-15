87aad51ebf38e7ade1597d4af889068567233bd7
gegetako <gemamartose@gmail.com>
2025-05-15 18:19:28 +0200

    Añadido inicio de sesion pop up en prueba y eliminado css de EstiloInciioSesion y html de Inicio de Sesion(porque ha sido insertado en el html de prueba directamente)

--- VSCODE/scripts/Prueba.js
+++ VSCODE/scripts/Prueba.js
@@ -4,3 +4,14 @@ window.addEventListener('scroll', function() {
   // Ajusta la velocidad cambiando el divisor (e.g. 2 para más lento, 0.5 para más rápido)
   background.style.transform = 'translateY(' + scrolled * 2 + 'px)';
 });
+
+
+function openLoginPopup(event) {
+    event.preventDefault(); // Evita que el enlace realice su acción predeterminada
+    document.getElementById('login-popup').style.display = 'flex'; // Muestra el pop-up
+}
+
+// Función para cerrar el pop-up
+function closeLoginPopup() {
+    document.getElementById('login-popup').style.display = 'none'; // Oculta el pop-up
+}
