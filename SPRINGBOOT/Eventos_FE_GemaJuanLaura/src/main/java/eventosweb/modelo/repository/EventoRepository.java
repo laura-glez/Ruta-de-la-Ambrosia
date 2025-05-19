dec3cfc3de6fe9c1c24d208e28b0969e3b2919d3
Juan Guevara <soytifon17@gmail.com>
2025-05-19 10:08:15 +0200

    ahora si que si

--- SPRINGBOOT/Eventos_FE_GemaJuanLaura/src/main/java/eventosweb/modelo/repository/EventoRepository.java
+++ SPRINGBOOT/Eventos_FE_GemaJuanLaura/src/main/java/eventosweb/modelo/repository/EventoRepository.java
@@ -1,11 +1,7 @@
 package eventosweb.modelo.repository;
 
 import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-
 import eventosweb.modelo.entities.Evento;
 
 public interface EventoRepository extends JpaRepository<Evento, Integer>{
--- SPRINGBOOT/Eventos_FE_GemaJuanLaura/src/main/java/eventosweb/modelo/repository/PerfilRepository.java
+++ SPRINGBOOT/Eventos_FE_GemaJuanLaura/src/main/java/eventosweb/modelo/repository/PerfilRepository.java
@@ -1,10 +1,7 @@
 package eventosweb.modelo.repository;
 
 import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
 import eventosweb.modelo.entities.Perfil;
 
 public interface PerfilRepository extends JpaRepository<Perfil, Integer>{
