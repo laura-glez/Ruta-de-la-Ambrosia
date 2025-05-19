1e81f63324502ca0e47961ce8cdc681ce61b34d3
laura-glez <glez.anton.laura@gmail.com>
2025-05-19 09:18:38 +0200

    base de datos con tres eventos de cata de vinos

--- EVENTOS_FEE.sql
+++ EVENTOS_FEE.sql
@@ -6,7 +6,12 @@ CREATE TABLE TIPOS
 NOMBRE VARCHAR(45) NOT NULL,
 DESCRIPCION VARCHAR(200)
 );
-
+insert into TIPOS ( NOMBRE, DESCRIPCION)
+		   values ( "Vinos", "Catas de vinos.");
+insert into TIPOS ( NOMBRE, DESCRIPCION)
+		   values ("Cervezas", "Catas de cerveza.");
+insert into TIPOS ( NOMBRE, DESCRIPCION)
+		   values ("Cócteles", "Catas de cócteles.");
 CREATE TABLE PERFILES
 (ID_PERFIL INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 NOMBRE VARCHAR(45) NOT NULL
@@ -48,6 +53,47 @@ ID_TIPO INT NOT NULL,
 FECHA_ALTA DATE,
 FOREIGN KEY(ID_TIPO) REFERENCES TIPOS(ID_TIPO)
 );
+
+insert into EVENTOS (nombre, DESCRIPCION,FECHA_INICIO,UNIDAD_DURACION,DURACION,DIRECCION,AFORO_MAXIMO,ESTADO,DESTACADO,PRECIO,ID_TIPO,FECHA_ALTA) 
+			 values ("Cata vinos Bodega Los Reyes",
+             "Experiencia groumet en la bodega Los Reyes", 
+             "2025-04-12","HORAS", 
+             2,
+             "Bodega de los reyes, calle de los reyes, 3", 
+             30, 
+             "ACTIVO", 
+             "S", 
+             28, 
+             1, 
+             "2025-05-19");
+             
+insert into EVENTOS (nombre, DESCRIPCION,FECHA_INICIO,UNIDAD_DURACION,DURACION,DIRECCION,AFORO_MAXIMO,ESTADO,DESTACADO,PRECIO,ID_TIPO,FECHA_ALTA) 
+			 values ("Vinos y quesos italianos premium en cata",
+             "Just Italia te ofrece un plan insuperable, relación calidad precio, una cata como si estuvieras en Italia, con 5 vinos, y una tabla con la mejor selección de 4 de los mejores quesos italianos.", 
+             "2025-04-12",
+             "HORAS", 
+             2,
+             "Just Italia, Calle de Castelló, 113", 
+             30, 
+             "ACTIVO", 
+             "S", 
+             28, 
+             1, 
+             "2025-05-19");
+             
+insert into EVENTOS (nombre, DESCRIPCION,FECHA_INICIO,UNIDAD_DURACION,DURACION,DIRECCION,AFORO_MAXIMO,ESTADO,DESTACADO,PRECIO,ID_TIPO,FECHA_ALTA) 
+			 values ("Cata de vinos con un experto enólogo",
+             "Disfruta y aprende sobre el vino en este curso de iniciación a la cata con el experto Jesús Flores Téllez, reconocido enólogo, sumiller y Premio Nacional de Gastronomía. ", 
+             "2025-04-12",
+             "HORAS", 
+             3,
+             "Aula Española del Vino Simancas, 5- C", 
+             30, 
+             "ACTIVO", 
+             "N", 
+             28, 
+             1, 
+             "2025-05-19");
 
 CREATE TABLE RESERVAS
 (ID_RESERVA INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
