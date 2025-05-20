bd9937ab41e0d847caab015e1c20d50dd94d2cf4
gema <IFPgemamartose@gmail.com>
2025-05-20 08:39:30 +0200

    yes

--- SPRINGBOOT/Eventos_FE_GemaJuanLaura/src/main/java/eventosweb/modelo/repository/EventoRepository.java
+++ SPRINGBOOT/Eventos_FE_GemaJuanLaura/src/main/java/eventosweb/modelo/repository/EventoRepository.java
@@ -2,6 +2,8 @@ package eventosweb.modelo.repository;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.DeleteMapping;
+
 import eventosweb.modelo.entities.Evento;
 
 public interface EventoRepository extends JpaRepository<Evento, Integer>{
