268817cde93ac42a6861c041f0c868f6823aea9c
gema <IFPgemamartose@gmail.com>
2025-05-22 14:30:17 +0200

    todo lo de ReservaDto creado falta crear el metodo en RestController

--- SPRINGBOOT/Eventos_FE_GemaJuanLaura/src/main/java/eventosweb/modelo/dto/ReservaDtoRepository.java
+++
@@ -1,7 +0,0 @@
-package eventosweb.modelo.dto;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReservaDtoRepository extends JpaRepository<ReservaDto, Integer> {
-
-}
